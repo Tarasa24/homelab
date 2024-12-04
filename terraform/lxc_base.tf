bf4746f1f92e8d04dc939493a4961a35dd1ed971
Tarasa24 <tarasa24@tarasa24.dev>
2024-12-04 22:13:11 +0100

    Update alpine base image

--- terraform/lxc_base.tf
+++ terraform/lxc_base.tf
@@ -2,5 +2,5 @@ resource "proxmox_virtual_environment_download_file" "alpine_linux_template" {
   content_type = "vztmpl"
   datastore_id = "USB-HDD"
   node_name    = "pve"
-  url          = "http://download.proxmox.com/images/system/alpine-3.18-default_20230607_amd64.tar.xz"
+  url          = "http://download.proxmox.com/images/system/alpine-3.20-default_20240908_amd64.tar.xz"
 }
