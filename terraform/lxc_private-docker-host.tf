4f13a0033596937e2e3b58f850657c584644bdc4
Tarasa24 <tarasa24@tarasa24.dev>
2026-03-31 10:49:40 +0200

    Add firefly iii

--- terraform/lxc_private-docker-host.tf
+++ terraform/lxc_private-docker-host.tf
@@ -55,7 +55,7 @@ resource "proxmox_virtual_environment_container" "lxc_private-docker-host" {
 
   disk {
     datastore_id = "local-lvm"
-    size         = 32
+    size         = 64
   }
 
   mount_point {
